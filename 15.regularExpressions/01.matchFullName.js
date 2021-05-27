914607f5df98581f7b55486a9c5011860433f266
Yavor Petkov <hermsues@gmail.com>
2021-05-27 15:52:07 +0300

    added the lab exercises

---
+++ 15.regularExpressions/01.matchFullName.js
@@ -0,0 +1,9 @@
+function fullName(string) {
+	let checker = /\b[A-Z][a-z]+ [A-Z][a-z]+/g;
+	let validNames = [];
+	while ((validName = checker.exec(string)) !== null) {
+		validNames.push(validName[0]);
+	}
+	console.log(validNames.join(' '));
+}
+fullName('Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov');
